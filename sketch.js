089da370de48158ce991929006d4350cf2da33cf
Orit Moskovich <orit.mosko@gmail.com>
2022-03-23 18:37:28 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,12 +7,10 @@ function preload() {
 
 function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
-  // canvas.position(0, 0);
-  // canvas.style('z-index', '-1');
 }
 
 function windowResized() {
-  // resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
